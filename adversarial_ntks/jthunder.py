ef016e693f103ff22050243c3ecde6298534bef4
Tony Wang <tong.t.wang@gmail.com>
2021-04-27 00:32:15 -0700

    Add jthunder

---
+++ adversarial_ntks/jthunder.py
@@ -0,0 +1,84 @@
+"""thundersvm.SVC in Jax"""
+
+import numpy as np
+import thundersvm
+
+import jax
+import jax.numpy as jnp
+
+from adversarial_ntks import kernel
+
+
+def sv_gram(clf: thundersvm.SVC, X: jnp.ndarray) -> jnp.ndarray:
+    """
+    Get gram matrix between support vectors and X.
+    X: shape (n_samples, n_features)
+    return: shape (n_SV, n_samples)
+
+    Based off of formulas at
+    https://scikit-learn.org/stable/modules/svm.html#svm-kernels
+    """
+    SV = jnp.array(clf.support_vectors_)  # of shape(n_SV, n_features)
+
+    # To handle thundersvm
+    if SV.shape[1] == 1 + X.shape[1]:
+        SV = SV[:, 1:]
+
+    #print(clf.support_vectors_[0, 1:])
+    #print(clf.support_[0])
+    """
+    print()
+    
+    print(clf.support_vectors_[0])
+    print(clf.support_[0])
+    print(clf.n_features)
+    print("HI!")
+    print(SV.shape)
+    exit()
+    """
+
+    if clf.kernel == "linear":
+        return kernel.linear(SV, X)
+    elif clf.kernel == "polynomial":
+        return kernel.poly(
+            SV,
+            X,
+            gamma=clf._gamma,
+            coef0=clf.coef0,
+            degree=clf.degree,
+        )
+    elif clf.kernel == "rbf":
+        return kernel.rbf(SV, X, gamma=clf._gamma)
+    elif callable(clf.kernel):
+        return clf.kernel(SV, X)
+    else:
+        raise NotImplementedError
+
+
+def decision_function(clf: thundersvm.SVC, X: jnp.ndarray) -> jnp.ndarray:
+    """
+    X: shape (n_samples, n_features)
+    return: shape (n_samples, )
+    """
+    gmat = sv_gram(clf, X)  # shape: (n_SV, n_samples)
+    dual_coefs = jnp.array(clf.dual_coef_)  # shape: (n_classes - 1, n_SV)
+    assert dual_coefs.shape[0] == 1
+    return -(dual_coefs @ gmat)[0] + clf.intercept_
+
+
+def _decision_function_sum(clf: thundersvm.SVC, X: jnp.ndarray) -> float:
+    return decision_function(clf, X).sum()
+
+
+grad_decision_function = jax.jit(
+    jax.grad(_decision_function_sum, 1),
+    static_argnums=0,
+)
+
+
+def predict(clf: thundersvm.SVC, X: jnp.ndarray) -> jnp.ndarray:
+    """
+    x: shape (n_features, )
+    return: shape (n_features, )
+    """
+    return (decision_function(clf, X) > 0).astype(np.int)
---
+++ adversarial_ntks/tests/test_jthunder.py
@@ -0,0 +1,66 @@
+import unittest
+
+import numpy as np
+import thundersvm
+
+import jax
+import jax.numpy as jnp
+
+from adversarial_ntks import jthunder
+from adversarial_ntks.dataset import get_np_data
+
+jax.config.update("jax_enable_x64", True)
+
+
+class TestJThunder(unittest.TestCase):
+    def setUp(self):
+        self.ds_train = get_np_data(
+            name="mnist",
+            split="train[:128]",
+            binary_labels=True,
+            image_width=14,
+        )
+
+        self.ds_test = get_np_data(
+            name="mnist",
+            split="test[:32]",
+            binary_labels=True,
+            image_width=14,
+        )
+        self.jtest_xs = jnp.array(self.ds_test.xs)
+
+    def check_jthunder(self, clf: thundersvm.SVC):
+        self.assertTrue(
+            np.allclose(
+                clf.decision_function(self.ds_test.xs).flatten(),
+                -jthunder.decision_function(clf, self.jtest_xs),
+                rtol=1e-4,
+                atol=1e-4,
+            ))
+
+        self.assertTrue(
+            all(
+                clf.predict(self.ds_test.xs).astype(np.int) ==
+                jthunder.predict(clf, self.jtest_xs)))
+
+    def test_linear(self):
+        self.check_jthunder(
+            thundersvm.SVC(C=3, kernel="linear").fit(
+                X=self.ds_train.xs,
+                y=self.ds_train.ys,
+            ))
+
+    def test_poly(self):
+        self.check_jthunder(
+            thundersvm.SVC(C=3, kernel="polynomial", coef0=np.pi,
+                           degree=2).fit(
+                               X=self.ds_train.xs,
+                               y=self.ds_train.ys,
+                           ))
+
+    def test_rbf(self):
+        self.check_jthunder(
+            thundersvm.SVC(C=3, kernel="rbf").fit(
+                X=self.ds_train.xs,
+                y=self.ds_train.ys,
+            ))
