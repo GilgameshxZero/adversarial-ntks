2a0926cdf6c59d5493543ecd970e8f16b7c39e02
Tony Wang <tong.t.wang@gmail.com>
2021-04-27 16:53:43 -0700

    Rewrite jthunder to avoid memory leak

--- adversarial_ntks/jthunder.py
+++ adversarial_ntks/jthunder.py
@@ -2,6 +2,7 @@
 thundersvm.SVC in Jax
 ThunderSVM is very jank.
 """
+from typing import NamedTuple
 
 import numpy as np
 import thundersvm
@@ -12,60 +13,113 @@ import jax.numpy as jnp
 from adversarial_ntks import kernel
 
 
-def sv_gram(clf: thundersvm.SVC, X: jnp.ndarray) -> jnp.ndarray:
+class _JThunderParams(NamedTuple):
+    """Everything here can be passed into jax as non static argument"""
+    gamma: float
+    coef0: float
+    degree: int
+    intercept: float
+
+    SV: jnp.ndarray
+    dual_coefs: jnp.ndarray
+
+
+def _get_params(clf: thundersvm.SVC) -> _JThunderParams:
+    assert clf.n_classes == 2
+
+    jtp = _JThunderParams(
+        gamma=clf._gamma,
+        coef0=clf.coef0,
+        degree=clf.degree,
+        intercept=clf.intercept_[0],
+        SV=jnp.array(clf.support_vectors_[:, 1:]),
+        dual_coefs=jnp.array(clf.dual_coef_),
+    )
+
+    assert jtp.SV.shape == (clf.n_support_.sum(), clf.n_features)
+    assert jtp.dual_coefs.shape == (1, clf.n_support_.sum())
+
+    return jtp
+
+
+def _sv_gram(
+    kernel_type: str,
+    jtp: _JThunderParams,
+    X: jnp.ndarray,
+) -> jnp.ndarray:
     """
     Get gram matrix between support vectors and X.
     X: shape (n_samples, n_features)
     return: shape (n_SV, n_samples)
 
     Based off of formulas at
-    https://scikit-learn.org/stable/modules/svm.html#svm-kernels
+    https://scikit-learn.org/stable/modules/svm.html#svm-kernel_types
     """
-    SV = jnp.array(clf.support_vectors_)  # of shape(n_SV, n_features)
-
-    # ThunderSVM sometimes gets shape wrong?!?!
-    if SV.shape[1] == 1 + X.shape[1]:
-        SV = SV[:, 1:]
-
-    if clf.kernel == "linear":
-        return kernel.linear(SV, X)
-    elif clf.kernel == "polynomial":
+    if kernel_type == "linear":
+        return kernel.linear(jtp.SV, X)
+    elif kernel_type == "polynomial":
         return kernel.poly(
-            SV,
+            jtp.SV,
             X,
-            gamma=clf._gamma,
-            coef0=clf.coef0,
-            degree=clf.degree,
+            gamma=jtp.gamma,
+            coef0=jtp.coef0,
+            degree=jtp.degree,
         )
-    elif clf.kernel == "rbf":
-        return kernel.rbf(SV, X, gamma=clf._gamma)
-    elif callable(clf.kernel):
-        return clf.kernel(SV, X)
+    elif kernel_type == "rbf":
+        return kernel.rbf(jtp.SV, X, gamma=jtp.gamma)
     else:
         raise NotImplementedError
 
 
-def decision_function(clf: thundersvm.SVC, X: jnp.ndarray) -> jnp.ndarray:
+def _decision_function(
+    kernel_type: str,
+    jtp: _JThunderParams,
+    X: jnp.ndarray,
+) -> jnp.ndarray:
     """
     X: shape (n_samples, n_features)
     return: shape (n_samples, )
     """
-    gmat = sv_gram(clf, X)  # shape: (n_SV, n_samples)
-    dual_coefs = jnp.array(clf.dual_coef_)  # shape: (n_classes - 1, n_SV)
-    assert dual_coefs.shape[0] == 1
+    gmat = _sv_gram(kernel_type, jtp, X)  # shape: (n_SV, n_samples)
 
     # Need to do weird sign change so positive is class 1.
-    return -(dual_coefs @ gmat)[0] + clf.intercept_
+    return -(jtp.dual_coefs @ gmat)[0] + jtp.intercept
 
 
-def _decision_function_sum(clf: thundersvm.SVC, X: jnp.ndarray) -> float:
-    return decision_function(clf, X).sum()
+def _decision_function_sum(kernel_type: str, jtp: _JThunderParams,
+                           X: jnp.ndarray) -> float:
+    return _decision_function(kernel_type, jtp, X).sum()
 
 
-grad_decision_function = jax.jit(
-    jax.grad(_decision_function_sum, 1),
+_grad_decision_function = jax.jit(
+    jax.grad(_decision_function_sum, 2),
     static_argnums=0,
 )
+
+
+def get_grad_decision_function(clf: thundersvm.SVC):
+    """
+    Written like this to prevent leaking memory.
+    We want static_argnums to be as light as possible.
+    https://github.com/google/jax/issues/282
+    """
+    kernel_type = clf.kernel
+    jtp = _get_params(clf)
+
+    def f(X):
+        return _grad_decision_function(kernel_type, jtp, X)
+
+    return f
+
+
+def decision_function(clf: thundersvm.SVC, X: jnp.ndarray) -> jnp.ndarray:
+    """
+    x: shape (n_features, )
+    return: shape (n_features, )
+    """
+    kernel_type = clf.kernel
+    jtp = _get_params(clf)
+    return _decision_function(kernel_type, jtp, X)
 
 
 def predict(clf: thundersvm.SVC, X: jnp.ndarray) -> jnp.ndarray:
